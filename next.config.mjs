de84d886599f7eaa671e3d0001d2eccdd86fa4f0
花隐 <huayin@MacBook-Pro.local>
2026-01-21 14:12:38 +0800

    chore: upgrade next/react and node config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 允许生产构建中存在 ESLint 错误
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // 允许生产构建中存在 TypeScript 错误
   typescript: {
     ignoreBuildErrors: true,
